a3fb0284587fc4cedeb0b2df6775070d067af8b5
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-02-06 11:02:42 -0600

    nothing

--- NODE JS/03-bases-node/multiplicar/multiplicar.js
+++ NODE JS/03-bases-node/multiplicar/multiplicar.js
@@ -4,19 +4,20 @@ const fs = require('fs')
 
 let crearArchivo = (base) => {
 
-        return new Promise
+        return new Promise((resolve, reject) => {
+            let data = '';
+
+            for (i = 1; i < 10; i++) {
+                data += `${base} * ${i} = ${base * i}\n`
+            }
+
+            fs.writeFile(`tabla-${base}`, data, (err) => {
+                if (err) throw err;
+                console.log('The file has been created');
+            });
+
+
+        })
 
     }
     // fs.writeFile(file,data[options],callback)
-
-
-let data = '';
-
-for (i = 1; i < 10; i++) {
-    data += `${base} * ${i} = ${base * i}\n`
-}
-
-fs.writeFile(`tabla-${base}`, data, (err) => {
-    if (err) throw err;
-    console.log('The file has been created');
-});
