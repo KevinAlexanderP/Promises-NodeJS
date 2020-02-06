bd656f07331789fd048d2399b68376ceecb281ea
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-02-06 14:02:59 -0600

    destructuracion

--- NODE JS/03-bases-node/app.js
+++ NODE JS/03-bases-node/app.js
@@ -1,4 +1,12 @@
-const multiplicar = require('./multiplicar/multiplicar')
+// const multiplicar = require('./multiplicar/multiplicar')
+
+// destructuracion 
+const { crearArchivo } = require('./multiplicar/multiplicar')
+let base = 4;
+
+crearArchivo(base)
+    .then(archivo => console.log(`Archivo creado ${archivo}`))
+
 
 // //file system en node js
 // crearArchivo(base)
